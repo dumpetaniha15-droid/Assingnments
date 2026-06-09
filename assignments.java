f84d6557e100572d2c5d98a5b0f5746a40b1c588
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-09 22:00:29 +0530

    added to assignments.java

---
+++ assignments.java
@@ -0,0 +1,15 @@
+public class assignments{
+    public static void main(String[]args){
+        int a = 10;
+        double b = 10.1;
+        String c = "kanna";
+        boolean d = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+    }
+}
