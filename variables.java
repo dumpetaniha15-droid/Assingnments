840555dfb5c316a6d31b59623c5a875dee28c2ad
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-09 21:14:57 +0530

    added to variables.java

--- variables.java
+++ variables.java
@@ -1,14 +1,15 @@
 public class variables {
-    public static void main(String[]args){
+    public static viod main(string[]args){
         int m = 15;
-        double n = 15.0;
-        string o = "niharika";
+        double n = 15.5;
+        string o = "NIHARIKA";
         boolean p = false;
 
         System.out.println(m);
         System.out.println(n);
         System.out.println(o);
         System.out.println(p);
+    
     }
     
 }
