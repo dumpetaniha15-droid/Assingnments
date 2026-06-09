4b4a3266ff1eb0e3d2e47a376db201b51e518ef1
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-09 21:27:12 +0530

    added to variables.java file

--- variables.java
+++ variables.java
@@ -1,14 +1,14 @@
 public class variables {
     public static void main(String[]args){
-        int a = 15;
-        double b = 15.5;
-        String c = "NIHARIKA";
-        boolean d = false;
+        int m = 15;
+        double n = 15.5;
+        String o = "NIHARIKA";
+        boolean p = false;
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
+        System.out.println(m);
+        System.out.println(n);
+        System.out.println(o);
+        System.out.println(p);
     
     }
     
