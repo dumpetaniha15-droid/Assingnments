072c75b36886dbf0339557b09d80d9e5a75b92f3
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-10 21:39:15 +0530

    added to datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,26 @@
+public class datatypes {
+    public static void main(String[] args){
+        int workers = 100;
+        char section = 'A';
+        float height = 5.7f;
+        double rent = 10000;
+        byte price = 120;
+        short books = 500;
+        long phoneNumber =7815944014l;
+        boolean pass = true;
+        
+        System.out.println(workers);
+        System.out.println(section);
+        System.out.println(height);
+        System.out.println(rent);
+        System.out.println(price);
+        System.out.println(books);
+        System.out.println(phoneNumber);
+        System.out.println(pass);
+
+
+
+
+    }
+    
+}
