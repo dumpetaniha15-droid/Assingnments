3c402a6db80d0d452be1f0d07d64e8cf3df499b6
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-12 10:20:31 +0530

    added to assignments.java

--- chinnari.java
+++ chinnari.java
@@ -1,9 +1,11 @@
 public class chinnari {
     public static void main(String[] args){
-        System.out.println("my name is Niharika");
-        System.out.println("iam a student in Nishitha degree college");
-        System.out.println("iam top 2 in my class");
-        System.out.println("i got two medals after completing my second year");
+        System.out.print("my name is Niharika,");
+        System.out.print("iam a student in Nishitha degree college.");
+        System.out.print("Iam top 2 in my class");
+        System.out.print("i got two medals after completing my second year,");
+        System.out.print("now iam in the final year");
+        System.out.print("and also learning java full stack course");
     
         
     }
