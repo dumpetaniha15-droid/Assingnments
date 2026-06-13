66ed59b93936668f4fe4616e7089b51301c93872
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-13 23:26:19 +0530

    added to variables3.java

--- variables3.java
+++ variables3.java
@@ -1,17 +1,17 @@
 public class variables3 {
+   
     static String Employee = "Niharika";
-    static double salary =35000;
-    static int age = 45;
+    static double salary = 45000;
+    static int age = 30;
     static int kilometers = 100;
-     public static void main(String[] args){
-        int age =25;
-        int kilometers =105;
+    public static void main(String[] args){
+        int age = 25;
+        int kilometers = 105;
 
         System.out.println(Employee);
         System.out.println(salary);
         System.out.println(age);
         System.out.println(kilometers);
-
     }
-    
+        
 }
