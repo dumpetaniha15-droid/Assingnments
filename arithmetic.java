6a2c4abcec1e7df67689c2a80ef27dc9c36de2be
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-15 22:41:41 +0530

    added  to arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,14 @@
+public class arithmetic {
+    public static void main(String[] args){
+        
+      int a=2 , b=3;
+
+      System.out.println(a + b);
+      System.out.println(a - b);
+      System.out.println(a * b);
+      System.out.println(a / b);
+      System.out.println(a % b);
+    }
+
+
+}
