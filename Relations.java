b4d6a1425141b920b972afaa107a0fb281668857
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-17 22:07:14 +0530

    added to Relations.java

---
+++ Relations.java
@@ -0,0 +1,17 @@
+public class Relations {
+    public static void main(String[] args){
+
+        int a = 20;
+        int b = 10;
+
+       System.out.println(a==b);  // (20==10) false
+       System.out.println(a!=b);  // (20!=10) true
+       System.out.println(a>b);   // (20>10)  true
+       System.out.println(a<b);   // (20<10)  false  
+       System.out.println(a>=b);  // (20>=10) true    20 not equal to 10 but, (20>10) so true
+       System.err.println(a<=b);  // (20<=10) false   20 not equal to 10 and also 20 not less than 10(20<10) so false  
+
+
+}
+    
+}
