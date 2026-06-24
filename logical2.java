d4cec39c9a36f2d2d59cfc22286af73641d6b759
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-24 15:36:53 +0530

    addded to logical2.java

---
+++ logical2.java
@@ -0,0 +1,11 @@
+public class logical2 {
+    public static void main(String[] args){
+        boolean isPassed = true;
+        boolean isFailed = false;
+
+        System.out.println(isPassed | isFailed);  //true
+        System.out.println(isPassed & isFailed);  //false
+    }   
+
+    
+}
