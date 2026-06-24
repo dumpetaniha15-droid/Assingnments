f6c097d58af6a8981f15b32012a7ea86b981bb8b
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-24 16:28:48 +0530

    added to logical1.java

---
+++ arithmetic2.java
@@ -0,0 +1,13 @@
+public class arithmetic2 {
+    public static void main(String[] args){
+        
+        int a = 4 , b = 3;
+
+        System.out.println(a + b);  //7
+        System.out.println(a - b);  //1
+        System.out.println(a * b);  //12
+        System.out.println(a / b);  //1
+        System.out.println(a % b);  //1
+    }
+    
+}
