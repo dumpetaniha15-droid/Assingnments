80ec76e7eaa5c19073f00d3e32af4fff957f43c0
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-24 17:06:34 +0530

    added to compoundassignment1.java

---
+++ compoundassignment1.java
@@ -0,0 +1,12 @@
+public class compoundassignment1 {
+    public static void main(String[] args){
+        int balence = 100;
+        System.out.println(balence);                    //100
+        System.out.println(balence = balence + 50);     //100+50=150
+        System.out.println(balence);                    //150  bcz balence is updated  
+        System.out.println(balence = balence - 50);     //150-50=100
+        System.out.println(balence);                    //100
+        
+    }
+    
+}
