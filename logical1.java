115e9200273ac0c5ec2672389b06ef3085c22dd8
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-24 15:19:57 +0530

    added to logical1.java

---
+++ logical1.java
@@ -0,0 +1,9 @@
+public class logical1 {
+    public static void main(String[] args){
+        boolean isOnline = true;
+        boolean isOfline = false;
+
+         System.out.println(isOnline || isOfline);   //false
+         System.out.println(isOnline && isOfline);   //true
+    }    
+}
