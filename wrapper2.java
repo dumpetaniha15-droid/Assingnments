9e8e9c57a1bfa080f7d0179d1e04d12181d85610
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-25 22:57:42 +0530

    added to wrapper2.java

---
+++ wrapper2.java
@@ -0,0 +1,35 @@
+public class wrapper2 {
+    public static void main(String[] args){
+
+        System.out.println("min value a int can hold:" + Integer.MIN_VALUE);
+        System.out.println("max value a int can hold:" + Integer.MAX_VALUE);
+        System.out.println("size of a int is:" + Integer.SIZE);
+
+        System.out.println("min value a char can hold:" + Character.MIN_VALUE );
+        System.out.println("max value a char can hold:" + Character.MIN_VALUE);
+        System.err.println("size of a char is:" + Character.SIZE);
+
+        System.out.println("min value a float can hold:" + Float.MIN_VALUE);
+        System.out.println("max value a float can hold:" + Float.MAX_VALUE);
+        System.out.println("size of a float is:" + Float.SIZE);
+
+        System.out.println("min value a double can hold:" + Double.MIN_VALUE);
+        System.out.println("max value a double can hold:" + Double.MAX_VALUE);
+        System.out.println("size of a double is:" + Double.SIZE);
+
+        System.out.println("min value a byte can hold:" + Byte.MIN_VALUE);
+        System.out.println("max value a byte can hold:" + Byte.MAX_VALUE);
+        System.out.println("size of a byte is:" + Byte.SIZE);
+
+        System.out.println("min value a short can hold:" + Short.MIN_VALUE);
+        System.out.println("max value a short can hold:" + Short.MAX_VALUE);
+        System.out.println("size of a short is:" + Short.SIZE);
+
+        System.out.println("min value a long can hold:" + Long.MIN_VALUE);
+        System.out.println("max value a long can hold:" + Long.MAX_VALUE);
+        System.out.println("size of a long is:" + Long.SIZE);
+
+        System.out.println(Boolean.TRUE);             // bcz Boolean has only two results :- (true or false)
+        System.out.println(Boolean.FALSE);
+    }
+}
