a5200b86999b6495fed92e3d5c5bfcb5d6e442de
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-26 23:06:44 +0530

    added to returntype2.java

---
+++ returntype2.java
@@ -0,0 +1,26 @@
+public class returntype2 {
+    public static void main(String[] args){
+        name("Niharika");
+        course("Bsc.computer science");
+        marks(90);
+     }
+
+     public static void name(String name){
+        System.out.println(name);
+     }
+
+     public static void course(String course){
+        System.out.println(course);
+    }
+
+     public static void marks(int marks){
+        System.out.println(marks);
+     }
+
+
+
+
+
+
+    
+}
