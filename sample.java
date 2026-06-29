93ba163916c135b7421a76ddb23aeffbee6bd905
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-29 22:49:16 +0530

    added to sample,java

---
+++ sample.java
@@ -0,0 +1,12 @@
+public class sample {
+
+    public static void main(String[] args){
+        greet();
+
+    }
+    public static void greet(){
+        System.out.println("hi everyone");
+    }
+
+}
+
