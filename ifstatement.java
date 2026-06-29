3434d145947135749b69411d5170d1244e2ce19e
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-29 22:32:10 +0530

    added to ifstatement.java

---
+++ ifstatement.java
@@ -0,0 +1,12 @@
+public class ifstatement {
+    static int age= 18;
+    public static void main(String[] args){
+        
+     if(age!=18){
+        System.out.println("she is major");
+     }else{
+         System.out.println("she is minior");
+
+    }
+}
+}
