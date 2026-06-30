318eda029d07e867559fa76a05c946cf91ef9f0f
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-06-30 11:36:03 +0530

    added to methods2.java

---
+++ mathods2.java
@@ -0,0 +1,26 @@
+public class mathods2 {
+    public static void main(String[] args){
+        System.out.println(add(10,10));
+        System.out.println(sub(5,3));
+        System.out.println(mull());
+        System.out.println(div(50,5));
+
+
+    }
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static int sub(int c,int d){
+        return c-d;
+    }
+    public static double mull(){
+        double e=4;
+        double f=2;
+        return e*f;
+    }
+    public static int div(int g,int h){
+        return g/h;
+    }
+    
+    
+}
