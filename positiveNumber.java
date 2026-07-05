5c61a3a83db49e8a0cc4bc8d123dd364062b1dd9
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-07-05 18:11:09 +0530

    added to switchweek.java

---
+++ positiveNumber.java
@@ -0,0 +1,8 @@
+public class positiveNumber {
+    public static void main(String[] args){
+        int number = 20;
+      if(number >0){
+      System.out.println("number is positive");
+    }
+}
+}
