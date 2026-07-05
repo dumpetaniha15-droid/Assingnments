2c3fae988a5fa5e42e8555452b846b6f2e97562c
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-07-05 17:11:23 +0530

    added to Ifweek.java

---
+++ Ifweek.java
@@ -0,0 +1,30 @@
+public class Ifweek {
+    public static void main(String[] args){
+        String day = "Saturday";
+
+        if(day == "Monday"){
+            System.out.println("Its a weekday");
+        }
+        else if(day == "Tuesday"){
+            System.out.println("Its a weekday");
+        }
+        else if(day == "Wednesday"){
+            System.out.println("Its a weekday");
+        }    
+        else if(day == "Thursday"){
+            System.out.println("Its a weekday");
+        }
+        else if(day == "Friday"){
+            System.out.println("Its a weekday");
+        }    
+        else if(day == "Saturday"){
+            System.out.println("Its a weekend");
+        }
+        else if(day == "Sunday"){
+            System.out.println("Its a weekend");
+        }else{
+            System.out.println("Invalid day");
+        }
+    }
+    
+}
