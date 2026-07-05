f72bf626e11bac4e1f12bb04a83a433db416c886
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-07-05 17:35:40 +0530

    added to switchweek.java

---
+++ switchweek.java
@@ -0,0 +1,33 @@
+public class switchweek {
+    public static void main(String[] args){
+        int day = 5;
+
+       switch (day){
+        case 1:
+            System.out.println("Its a weekday");
+            break;
+        case 2:
+            System.out.println("Its a weekday");
+            break;
+        case 3:   
+            System.out.println("Its a weekday");
+            break; 
+        case 4:
+            System.out.println("Its a weekday");
+            break; 
+        case 5:
+            System.out.println("Its a weekday");
+            break;
+        case 6:    
+            System.out.println("Its a weekend");
+            break;
+        case 7:
+            System.out.println("Its a weekend");
+            break;    
+        default :
+        System.out.println("Invalid day");
+                
+       }
+    }
+    
+}
