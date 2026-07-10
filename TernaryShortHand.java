c902d21fb96db84056c56c34715c008938b12bf4
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-07-10 18:10:37 +0530

    Ternary short hand

---
+++ TernaryShortHand.java
@@ -0,0 +1,18 @@
+public class TernaryShortHand {
+     public static void main(String[] args){
+       int number = 15;
+       int num = 12;
+       int age=19;
+       int marks=75;
+
+        System.out.println(number>0 ? "positive number" : "negative number");       
+        System.out.println(num %2 ==0 ? "even number" : "odd number");
+        System.out.println(age>=18 ? "eligible to vote" : "not eligible to vote");
+        System.out.println(marks>=35 ? "pass" : "fail");
+   }
+}
+
+ 
+
+   
+    
