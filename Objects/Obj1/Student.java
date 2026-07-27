11119ea910040c6c6c78ad74ccbb7181467b4e04
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-07-27 20:31:24 +0530

    objects and constuctors

--- Objects/Obj1/Student.java
+++ Objects/Obj1/Student.java
@@ -13,37 +13,8 @@ Student(String name, int age){
 
 void study(){
     System.out.println(name + " is practicing java");
-    System.out.println(name + " is learning");
+    System.out.println(name + " working hard");
     System.out.println(name + " scored good marks");
 }
-
-public char[] getName() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getName'");
 }
 
-public char[] getRollNo() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getRollNo'");
-}
-
-public char[] getMarks() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getMarks'");
-}
-
-public void setName(String string) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'setName'");
-}
-
-public void setrollNo(int i) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'setrollNo'");
-}
-
-public void setMarks(int i) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'setMarks'");
-}
-}
