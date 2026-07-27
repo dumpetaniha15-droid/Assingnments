a54f196a76a5049db494b439506ede312c00c389
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-07-27 14:37:48 +0530

    objects and constuctors

---
+++ Objects/Obj4/Dog.java
@@ -0,0 +1,20 @@
+package Objects.Obj4;
+
+public class Dog {
+    String name;
+    int age;
+    
+    void intro(){
+        System.out.println("my name is " + name);
+        System.out.println("iam " + age + " years old");
+    }
+    void eat(){
+        System.out.println(name + " is eating");
+    }
+    void play(){
+        System.out.println(name +" is playing with niharika");
+    }
+    void sleep(){
+        System.out.println(name + " is sleeping");
+    }
+}
---
+++ Objects/Obj4/dogData.java
@@ -0,0 +1,15 @@
+package Objects.Obj4;
+
+public class dogData {
+    public static void main(String[] args){
+
+        Dog d1 = new Dog();
+        d1.name = "lucky";
+        d1.age = 2;
+
+        d1.intro();
+        d1.eat();
+        d1.play();
+        d1.sleep();
+    }       
+}
