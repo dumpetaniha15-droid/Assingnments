98995d0d097331d1152a9cc5a0e9e3496b2a7878
Niharika Dumpeta <dumpetaniha15@gmail.com>
2026-08-04 13:17:53 +0530

    prg on covering all oops concepts

---
+++ oops/main.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class main {
+    public static void main(String[] args){
+
+        ClgStudent c = new ClgStudent("Niharika", 7, 94);
+        c.display();
+    }
+    
+}
---
+++ oops/student.java
@@ -0,0 +1,38 @@
+package oops;
+
+//parent class:
+abstract class person{
+    private String name;
+    private int rollNo;
+
+    person(String name, int rollNo) {   //Parameterized Constructor
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+    public String getName() {  //getters(encapsulation)
+        return name;
+    }
+    public int getRollNo() {
+        return rollNo;
+    }
+    abstract void display();   //abstract method (polymorphism)
+}
+
+// Child Class: (inheritance)
+class ClgStudent extends person{
+    private int marks;
+
+    ClgStudent(String name, int rollNumber, int marks) {   //Parameterized Constructor
+        super(name, rollNumber);
+        this.marks = marks;
+    }
+//polymorphism: override
+    void display() {
+        System.out.println("Name: " + getName());
+        System.out.println("Roll Number: " + getRollNo());
+        System.out.println("Marks: " + marks);
+    }
+}
+
+    
+
